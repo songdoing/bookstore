fbf2099ad04f608f69f8a8644c3fed950beb63f8
wonjinsong <msmk22239@gmail.com>
2022-04-03 18:31:26 -0400

    register post type

--- wp-content/themes/jenny-bookstore/functions.php
+++ wp-content/themes/jenny-bookstore/functions.php
@@ -2,4 +2,13 @@
 function getHelloWorld(){
     return '<h1>Hello World! Jenny Song!</h1>';
 }
+
+function registerPostType(){
+    register_post_type('book', [
+        'public' => true,
+        'label' => 'Books',
+    ]);
+}
+add_action('init', 'registerPostType'); 
+
 ?>
