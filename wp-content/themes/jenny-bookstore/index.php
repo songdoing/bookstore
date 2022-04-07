350366b152d621d979a18b68d9d33cb2d7e9f954
wonjinsong <msmk22239@gmail.com>
2022-04-07 08:12:43 -0400

    get_post_type_archive_link

--- wp-content/themes/jenny-bookstore/index.php
+++ wp-content/themes/jenny-bookstore/index.php
@@ -1,10 +1,18 @@
+<div>
+    <a href=" <?php get_post_type_archive_link('book') ?>"> Book List </a>
+</div>
+
 <ul>
 <?php 
 if(have_posts()) {
     while(have_posts()) {
         the_post();
 ?>
-    <li><?php the_title(); ?> </li>
+    <li>
+        <a href="<?php the_permalink(); ?> ">    
+            <?php the_title(); ?> 
+        </a>
+    </li>
 <?php        
     }
 }
